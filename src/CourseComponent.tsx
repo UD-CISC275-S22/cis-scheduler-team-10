6a1f9f5e1415d7327c69c956c2095cc73b571883
Katie <kehoyt@udel.edu>
2022-04-15 13:08:00 -0400

    edit mode working for courses

--- src/CourseComponent.tsx
+++ src/CourseComponent.tsx
@@ -1,19 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form, FormGroup } from "react-bootstrap";
 import { Course } from "./interfaces/course";
 
+type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
+
 export function CourseComponent({ course }: { course: Course }): JSX.Element {
+    const [editMode, changeEditMode] = useState<boolean>(false);
+    const [courseCode, changeCode] = useState<string>(course.courseCode);
+    const [courseTitle, changeTitle] = useState<string>(course.courseTitle);
+    const [credits, changeCredits] = useState<number>(course.numCredits);
+    const [currentCourse, updateCourse] = useState<Course>(course);
+
     return (
         <div
             // className="course"
             style={{ border: "1px solid black", padding: "10px" }}
         >
-            <div>
-                <b>{course.courseCode}</b>
-            </div>
-            <div>
-                <i>{course.courseTitle}</i>
-            </div>
-            <p> {course.courseDescription}</p>
+            <FormGroup>
+                {!editMode && (
+                    <div>
+                        <b>{courseCode}</b>
+                    </div>
+                )}
+                {editMode && (
+                    <Form.Control
+                        type="textbox"
+                        value={courseCode}
+                        onChange={(event: ChangeEvent) =>
+                            changeCode(event.target.value)
+                        }
+                    ></Form.Control>
+                )}
+                {!editMode && (
+                    <div>
+                        <i>{courseTitle}</i>
+                    </div>
+                )}
+                {editMode && (
+                    <Form.Control
+                        type="textbox"
+                        value={courseTitle}
+                        onChange={(event: ChangeEvent) =>
+                            changeTitle(event.target.value)
+                        }
+                    ></Form.Control>
+                )}
+                {!editMode && <p> {course.courseDescription}</p>}
+                {editMode && (
+                    <Form.Control
+                        type="textbox"
+                        value={credits}
+                        onChange={(event: ChangeEvent) =>
+                            changeCredits(parseInt(event.target.value))
+                        }
+                    ></Form.Control>
+                )}
+                {!editMode && (
+                    <Button
+                        onClick={() => {
+                            changeEditMode(!editMode);
+                        }}
+                    >
+                        Edit
+                    </Button>
+                )}
+                {editMode && (
+                    <Button
+                        onClick={() => {
+                            updateCourse({
+                                ...currentCourse,
+                                courseCode: courseCode,
+                                courseTitle: courseTitle,
+                                numCredits: credits
+                            });
+                            changeEditMode(!editMode);
+                        }}
+                    >
+                        Save
+                    </Button>
+                )}
+                {editMode && (
+                    <Button
+                        onClick={() => {
+                            changeCode(course.courseCode);
+                            changeTitle(course.courseTitle);
+                            changeCredits(course.numCredits);
+                            changeEditMode(!editMode);
+                        }}
+                    >
+                        Cancel
+                    </Button>
+                )}
+            </FormGroup>
         </div>
     );
 }
