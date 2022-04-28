ed6153501da99eaee8ea6c39f87a747ffa805804
Katie <kehoyt@udel.edu>
2022-04-27 20:20:57 -0400

    changing requirement types to match catalog

--- src/interfaces/course.ts
+++ src/interfaces/course.ts
@@ -1,6 +1,21 @@
 // Provides a Framework for a Single Course
 
-export type RequirementType = "university" | "college" | "major";
+export type RequirementType =
+    | "University: "
+    | "A&S: "
+    | "University: Social and Behavioral Sciences"
+    | "University: Social and Behavioral Sciences (SOC &amp; BESC)"
+    | "University: Mathematics, Natural Sciences and Technology"
+    | "University: Mathematics, Natural Sciences, and Technology (MTH NS &amp; T)"
+    | "University: Creative Arts and Humanities"
+    | "University: Creative Arts and Humanities (ART &amp; HUM)"
+    | "University: History and Cultural Change"
+    | "University: History and Cultural Change (HIST &amp; CLT)"
+    | "A&S: GROUP A: A&S Creative Arts & Humanities"
+    | "A&S: GROUP B: A&S History & Cultural Change"
+    | "A&S: GROUP C: A&S Social & Behavioral Sci"
+    | "A&S: GROUP D: A&S Math, Nat Sci & Technology";
+// split
 
 export interface Course {
     // Code (string)
